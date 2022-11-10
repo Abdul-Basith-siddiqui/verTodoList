b7bbe8edf3702b9f23759af2f2b31306b783bf3a
Abdul Basith <mohdabdulbasith17@gmail.com>
2022-11-10 14:49:45 -0800

    ver1 commit 2

--- app.js
+++ app.js
@@ -1,52 +1,35 @@
 const express = require("express"); //adding modules
 const bodyParse = require("body-parser"); //adding modules
 
+let userInputs=["Buy the food","cook the food","eat the food"];
 const app = express();
 app.set('view engine', 'ejs'); //view engine is very popular in node // have to create "view" folder and "filename.ejs" file to use this engine
-//don't change the above line 
+//don't change the above line
+
+app.use(bodyParse.urlencoded({extended:true}));
 
 const today = new Date();
-const currentDay = today.getDay();
+// const currentDay = today.getDay();
 app.get("/", function(req, res) { // creating the get home route
-  //  res.send("hello");
-  let day = "";
-  switch (currentDay) {
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thusday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    case 0:
-      day = "Sunday";
-      break;
-    default :
-      console.log(currentDay);
-  }
-  // if(currentDay===7 || currentDay===0){
-  //   // res.send("holiday");
-  //   day="weekend";
-  // }else{
-  //   day="week day"
-  //   // res.write("<p>go to work</p>");
-  //   // res.write("<h1>boo!</h1>");
-  //   // res.sendFile(__dirname+"/index.html");
-  // }
+  let options={
+    weekday:"long",
+    month: "long",
+    year:"numeric",
+    day:"numeric"
+  };
 
-  res.render("list", {todayIs: day});  //render() method uses the view engine to render a particular page
+  let day=today.toLocaleDateString("ja-JP",options);  // en-US
+  res.render("list", {todayIs: day, newli:userInputs});  //render() method uses the view engine to render a particular page
+  // html escaped ek saat mangta iine res.render up
 });
+
+
+  app.post("/",function(req,res){   //when the request revieve app.post caught the request
+    let userInput=req.body.data;  //digging to access the value by req.body.(name of the forms to get the value)
+     userInputs.push(userInput);   //appending userInput into array
+    // res.send(userInput);            //sending to root route display
+    res.redirect("/");               //redirect to root route up app.get("/"...)
+  });
 
 
 
